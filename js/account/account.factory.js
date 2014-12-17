7e9030495eba8d6b638443720cb8113b310d82f7
jessicafraines <jessicafraines@gmail.com>
2014-12-16 23:13:22 -0600

    added account factory

--- js/account/account.factory.js
+++ js/account/account.factory.js
@@ -2,6 +2,8 @@
   'use strict';
   angular.module('mommyApp')
   .factory('accountFactory', function($rootScope, $location, FIREBASE_URL){
+    var accountFactory = {},
+        ref = new Firebase(FIREBASE_URL);
   
   }) //close factory
 }());
