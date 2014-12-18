9ddcfbb766c6383340e0286e126019b7cfe316f6
jessicafraines <jessicafraines@gmail.com>
2014-12-18 16:25:32 -0600

    moved functions to edit a childs info to show controller

--- js/child/child.ctrl.js
+++ js/child/child.ctrl.js
@@ -16,20 +16,45 @@
           vm.children[data.name] = vm.newChild;
         });
       };
+    }) //closes child controller
+
+    .controller('EditController', function($routeParams, childFactory){
+      var vm = this,
+          id = $routeParams.childId,
+      apptId = $routeParams.apptId;
+
+      childFactory.getAppt(id, apptId, function(data){
+        vm.newAppt = data;
+      });
+
+      vm.addItem = function(apptId){
+        childFactory.editItem(id, apptId, vm.newAppt);
+      };
+    }) //closes edit controller
+    .controller('ShowController', function($routeParams, $location, childFactory){
+      var vm = this,
+          id = $routeParams.childId;
+    vm.childId = id;
+
+      childFactory.getChild(id, function(data){
+        vm.child = data;
+      });
       vm.addAppt = function(){
         childFactory.createAppt(id, vm.newAppt, function(data){
           vm.appts = vm.appts || {};
           vm.appts[data.date] = vm.newAppt;
+          $location.path('/children/:id');
         });
       };
       childFactory.getAppts(id, function(data){
         vm.appts = data;
       });
-    }) //closes child controller
 
-   /* .controller('ChildrenController', function(childFactory){
-      var vm = this;
+      vm.deleteAppt = function(apptId){
+        childFactory.deleteAppt(id, apptId, function(){
+          delete vm.appts[apptId];
+        });
+      };
 
-
-    }) //closes children controller*/
+    }) //closes show controller
 }());
--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 ;(function(){
  'use strict';
  angular.module('mommyApp')
-  .constant('FIREBASE_URL', 'https://mommy-business.firebaseio.com/')
+  .constant('FIREBASE_URL', 'https://mommy-business.firebaseio.com')
 }());
