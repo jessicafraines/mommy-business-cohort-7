1f10203296fc4110b41a3ffeb19d0903fd134bbe
jessicafraines <jessicafraines@gmail.com>
2014-12-18 22:42:59 -0600

    added growth and milestone functions

--- js/child/child.ctrl.js
+++ js/child/child.ctrl.js
@@ -18,20 +18,36 @@
       };
     }) //closes child controller
 
-    .controller('EditController', function($routeParams, childFactory){
-      var vm = this,
-          id = $routeParams.childId,
-      apptId = $routeParams.apptId;
+    .controller('EditController', function($location, $routeParams, childFactory){
+      var vm      = this,
+          id      = $routeParams.childId,
+      apptId      = $routeParams.apptId,
+      growthId    = $routeParams.growthId,
+      milestoneId = $routeParams.milestoneId;
 
+      childFactory.getMilestone(id, milestoneId, function(data){
+        vm.newMilestone = data;
+      });
+      vm.addMilestone = function(){
+        childFactory.editMilestone(id, milestoneId, vm.newMilestone);
+        $location.path('/children/' + id);
+      };
+      childFactory.getGrowth(id, growthId, function(data){
+        vm.newGrowth = data;
+      });
+      vm.addGrowth = function(){
+        childFactory.editGrowth(id, growthId, vm.newGrowth);
+        $location.path('/children/' + id);
+      };
       childFactory.getAppt(id, apptId, function(data){
         vm.newAppt = data;
       });
-
-      vm.addItem = function(apptId){
-        childFactory.editItem(id, apptId, vm.newAppt);
+      vm.addAppt = function(){
+        childFactory.editAppt(id, apptId, vm.newAppt);
+        $location.path('/children/' + id);
       };
     }) //closes edit controller
-    .controller('ShowController', function($routeParams, $location, $scope, childFactory){
+    .controller('ShowController', function($routeParams, $location,  childFactory){
       var vm = this,
           id = $routeParams.childId;
     vm.childId = id;
@@ -39,12 +55,44 @@
       childFactory.getChild(id, function(data){
         vm.child = data;
       });
+      vm.addMilestone = function(){
+        childFactory.createMilestone(id, vm.newMilestone, function(data){
+          vm.milestones = vm.milestones || {};
+          vm.milestones[data.date] = vm.newMilestone;
+          $location.path('/children/' + id);
+        });
+      };
+      childFactory.getMilestones(id, function(data){
+        console.log('Controller');
+        vm.milestones = data;
+      });
+
+      vm.deleteMilestone = function(milestoneId){
+        childFactory.deleteMilestone(id, milestoneId, function(){
+          delete vm.milestones[milestoneId];
+        });
+      };
+      vm.addGrowth = function(){
+        childFactory.createGrowth(id, vm.newGrowth, function(data){
+          vm.growths = vm.growths || {};
+          vm.growths[data.date] = vm.newGrowth;
+          $location.path('/children/' + id);
+        });
+      };
+      childFactory.getGrowths(id, function(data){
+        vm.growths = data;
+      });
+
+      vm.deleteGrowth = function(growthId){
+        childFactory.deleteGrowth(id, growthId, function(){
+          delete vm.growths[growthId];
+        });
+      };
       vm.addAppt = function(){
         childFactory.createAppt(id, vm.newAppt, function(data){
           vm.appts = vm.appts || {};
           vm.appts[data.date] = vm.newAppt;
           $location.path('/children/' + id);
-          //$scope.$apply();
         });
       };
       childFactory.getAppts(id, function(data){
